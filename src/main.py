92e2c1048c9637fa4ac7bd88ea55d18e7c61027a
DarkChild <caiopcmx@gmail.com>
2026-06-11 08:52:53 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from src.routes import eventos, campeonatos, participantes, inscricoes
+from routes import eventos, campeonatos, participantes, inscricoes
 
 app = FastAPI(title="EventHub API", version="1.0")
 
-# Inclui os seus 4 CRUDs completos de volta no sistema
 app.include_router(eventos.router)
 app.include_router(campeonatos.router)
 app.include_router(participantes.router)
